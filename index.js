5019e1d7248ff3e3641b7213b895a98bc8403e62
Kenny <101839505+khui0@users.noreply.github.com>
2022-07-19 17:22:54 -0400

    Add command line arguments using minimist

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const arguments = process.argv.slice(2);
+const argv = require('minimist')(process.argv.slice(2), {
+    boolean: ["auto", "remove-null"],
+    alias: {
+        w: "width",
+        h: "height"
+    }
+});
 
 const fs = require("fs");
 const path = require("path");
 const { createCanvas } = require("canvas");
 const Jimp = require("jimp-compact");
 
-const w = 64;
-const h = 64;
-
-switch (path.parse(arguments[0]).ext.toLowerCase()) {
-    case ".txt":
-        fs.readFile(arguments[0], "utf8", (err, data) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            fs.writeFileSync(replaceExt(arguments[0], "png"), toImage(data));
-            console.log(`Converted "${path.basename(arguments[0])}" into an image [${(data.length / (w * h * 3) * 100).toFixed(2)}%][${data.length}/${w * h * 3}]`);
-        });
-        break;
-    case ".png":
-        Jimp.read(arguments[0], (err, data) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            fs.writeFileSync(replaceExt(arguments[0], "txt"), toText(data));
-            console.log(`Converted "${path.basename(arguments[0])}" into a text file`);
-        });
-        break;
+if (argv._.length > 0) {
+    switch (path.parse(argv._[0]).ext.toLowerCase()) {
+        case ".txt":
+            fs.readFile(argv._[0], "utf8", (err, data) => {
+                if (err) throw err;
+                let w;
+                let h;
+                if (argv["auto"]) {
+                    w = h = Math.ceil(Math.sqrt(data.length / 3));
+                }
+                else {
+                    w = argv["w"] || 64;
+                    h = argv["h"] || 64;
+                }
+                fs.writeFileSync(replaceExt(argv._[0], "png"), toImage(data, w, h));
+                console.log(`Converted "${path.basename(argv._[0])}" into an image [${w}x${h}][${(data.length / (w * h * 3) * 100).toFixed(2)}%][${data.length}/${w * h * 3}]`);
+            });
+            break;
+        case ".png":
+            Jimp.read(argv._[0], (err, data) => {
+                if (err) throw err;
+                fs.writeFileSync(replaceExt(argv._[0], "txt"), toText(data, argv["remove-null"]));
+                console.log(`Converted "${path.basename(argv._[0])}" into a text file`);
+            });
+            break;
+    }
 }
 
-function toImage(string) {
+function toImage(string, w, h) {
     const canvas = createCanvas(w, h);
     const ctx = canvas.getContext("2d");
     let codes = [];
@@ -51,17 +59,17 @@ function toImage(string) {
     return canvas.toBuffer("image/png");
 }
 
-function toText(image) {
-    let w = image.bitmap.width;
-    let h = image.bitmap.height;
+function toText(image, removeNull) {
+    const w = image.bitmap.width;
+    const h = image.bitmap.height;
     let codes = [];
     let string = "";
     for (let y = 0; y < h; y++) {
         for (let x = 0; x < w; x++) {
             let rgba = Jimp.intToRGBA(image.getPixelColor(x, y));
-            (rgba.r != 0) && codes.push(rgba.r);
-            (rgba.g != 0) && codes.push(rgba.g);
-            (rgba.b != 0) && codes.push(rgba.b);
+            (rgba.r != 0 || !removeNull) && codes.push(rgba.r);
+            (rgba.g != 0 || !removeNull) && codes.push(rgba.g);
+            (rgba.b != 0 || !removeNull) && codes.push(rgba.b);
         }
     }
     for (let i = 0; i < codes.length; i++) {
